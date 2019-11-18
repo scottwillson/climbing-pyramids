12b4c116430ca65b26b2faeb8282d1aaab52b623
Scott Willson <scott.willson@gmail.com>
2019-11-18 07:50:04 -0800

    Use Bootstrap buttons for pyramid climbs

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,11 @@
 module HomeHelper
   def pyramid_climb_class(climb)
     if climb.sent? && climb.goal?
-      "sent goal"
+      "sent goal btn-success"
     elsif climb.sent?
-      "sent"
+      "sent btn-info"
+    else
+      "btn-light"
     end
   end
 end
